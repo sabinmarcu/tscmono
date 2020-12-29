61e4c972d99ce32b5206eb2937313cc745f8d192
Sabin Marcu <sabinmarcu@gmail.com>
2020-12-29 08:26:41 +0200

    feat: implement config load for repo

--- packages/repo/src/index.ts
+++ packages/repo/src/index.ts
@@ -1,12 +1,53 @@
 import { loadConfig, JSONSchema } from '@tscmono/config';
 import repoSchema from '@tscmono/config/schemas/repo.json';
+import type { RepoConfig } from '@tscmono/config/types/repo';
 
-(async () => {
-  console.log(
-    await loadConfig(
+/**
+ * @ignore
+ */
+let cache: RepoConfig;
+
+export const getConfig = async (): Promise<RepoConfig> => {
+  if (!cache) {
+    const config = await loadConfig<RepoConfig>(
       repoSchema as JSONSchema,
       __dirname,
       'tscmono',
-    ),
+    );
+    cache = config;
+  }
+  return cache;
+};
+
+(async () => {
+  let start = Date.now();
+  console.log('start');
+  console.log(
+    await getConfig(),
   );
+  console.log('end', Date.now() - start);
+  start = Date.now();
+  console.log('second');
+  console.log(
+    await getConfig(),
+  );
+  console.log('second', Date.now() - start);
+  start = Date.now();
+  console.log('second');
+  console.log(
+    await getConfig(),
+  );
+  console.log('second', Date.now() - start);
+  start = Date.now();
+  console.log('second');
+  console.log(
+    await getConfig(),
+  );
+  console.log('second', Date.now() - start);
+  start = Date.now();
+  console.log('second');
+  console.log(
+    await getConfig(),
+  );
+  console.log('second', Date.now() - start);
 })();

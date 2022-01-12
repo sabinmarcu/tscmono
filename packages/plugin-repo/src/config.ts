dcface916d6957f3f2377781cc474e3b75f4e2a2
Sabin Marcu <sabinmarcu@gmail.com>
2022-01-13 01:49:41 +0200

    feat: implement loose mode

--- packages/plugin-repo/src/config.ts
+++ packages/plugin-repo/src/config.ts
@@ -3,6 +3,9 @@ import repoSchema from '@tscmono/config/schemas/root.json';
 import type { WorkspaceRootConfig } from '@tscmono/config/schemas-types/root';
 import { registerCache } from '@tscmono/utils';
 
+import workspaceSchema from '@tscmono/config/schemas/workspace.json';
+import type { WorkspaceConfig } from '@tscmono/config/schemas-types/workspace';
+
 /**
  * Obtain the monorepo `tscmono` config using cosmiconfig
  * @param rootDir Optional directory from which to obtain the repo config
@@ -23,4 +26,17 @@ const getRepoConfig = (
 export const repoConfig = registerCache(
   'repoConfig',
   getRepoConfig,
+);
+
+/**
+ * Obtain the monorepo `tscmono` config using cosmiconfig
+ * @param rootDir Optional directory from which to obtain the repo config
+ * @category Config
+ */
+export const getConfig = (
+  rootDir: string = process.cwd(),
+) => loadConfig<WorkspaceConfig>(
+  workspaceSchema as JSONSchema,
+  'tscmono',
+  rootDir,
 );

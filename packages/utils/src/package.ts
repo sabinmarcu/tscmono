4e61a34aa44d294a744f9fb32e42d3de43312d1d
Sabin Marcu <sabinmarcu@gmail.com>
2022-06-27 19:48:52 +0300

    feat(utils): support package.ya?ml files

--- packages/utils/src/package.ts
+++ packages/utils/src/package.ts
@@ -1,28 +1,53 @@
 import fs from 'fs';
 import path from 'path';
+import { parse } from 'yaml';
+
+const packageOptions = {
+  'package.json': JSON.parse,
+  'package.yml': parse,
+  'package.yaml': parse,
+} as const;
 
 /**
  * Load a `package.json` file from a path
  * @param pwd The path
  * @category Util
  */
-export const getPackageJson = (pwd: string) => JSON.parse(
-  fs.readFileSync(
-    path.resolve(
-      pwd.replace(/\/package\.json$/, ''),
-      'package.json',
-    ),
-    'utf-8',
-  ),
-);
+export const getPackageJson = (pwd: string) => {
+  const attempts = Object.entries(packageOptions).map(
+    ([file, parser]) => {
+      try {
+        const contents = fs.readFileSync(
+          path.resolve(
+            pwd.replace(new RegExp(`/${file}$`), ''),
+            file,
+          ),
+          'utf-8',
+        );
+        return (parser as any)(contents);
+      } catch (e) {
+        return undefined;
+      }
+    },
+  );
+  const match = attempts.find(Boolean);
+  if (!match) {
+    throw new Error(`Could not find package.json at path ${pwd}`);
+  }
+  return match;
+};
 
 /**
  * Determine if path contains a `package.json` file
  * @param pwd Path
  * @category Util
  */
-export const hasPackageJson = (pwd: string) => fs.existsSync(
-  path.resolve(pwd, 'package.json'),
+export const hasPackageJson = (pwd: string) => Object.keys(
+  packageOptions,
+).some(
+  (test) => fs.existsSync(
+    path.resolve(pwd, test),
+  ),
 );
 
 /**
